90e3630586cd7c41810cb947b0ce3c40d0e5bea8
phisikus <phisikus@platinum.linux.pl>
2016-03-04 02:10:53 +0100

    Added Purchase model & fields class.

---
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/constants/PurchaseFields.java
@@ -0,0 +1,15 @@
+package com.qcadoo.mes.basic.constants;
+
+
+public final class PurchaseFields {
+  public static final String PRODUCT = "product";
+  public static final String QUANTITY = "quantity";
+  public static final String PRICE = "price";
+  public static final String DATE = "date";
+
+  private PurchaseFields() {
+
+  }
+
+
+}
