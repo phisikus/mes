9c575e3e7dfe667fc6ad212d0c893926cf6984f0
phisikus <phisikus@platinum.linux.pl>
2016-03-05 13:52:38 +0100

    Added average purchase price calculation & display.

---
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/PurchaseService.java
@@ -0,0 +1,42 @@
+package com.qcadoo.mes.basic;
+
+import com.qcadoo.mes.basic.constants.BasicConstants;
+import com.qcadoo.mes.basic.constants.PurchaseFields;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchResult;
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class PurchaseService {
+
+  final private String purchasesListUrl = "../page/basic/purchasesList.html";
+
+  @Autowired
+  private DataDefinitionService dataDefinitionService;
+
+  public void getAveragePriceOfAllPurchases(final ViewDefinitionState view, final ComponentState componentState, final String[] args) {
+    SearchResult resultList = dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_PURCHASE).find().list();
+    List<Entity> purchases = resultList.getEntities();
+    BigDecimal averagePrice = getAveragePrice(purchases);
+    String url = purchasesListUrl + "?averagePrice=" + averagePrice;
+    view.redirectTo(url, false, true);
+  }
+
+  private BigDecimal getAveragePrice(List<Entity> purchases) {
+    BigDecimal sum = new BigDecimal(0L);
+    BigDecimal size = new BigDecimal(purchases.size());
+    for (Entity purchase : purchases) {
+      sum = sum.add(purchase.getDecimalField(PurchaseFields.PRICE));
+    }
+    BigDecimal average = sum.divide(size);
+    return average;
+  }
+
+}
